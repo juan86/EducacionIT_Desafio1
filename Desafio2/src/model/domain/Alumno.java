2a1066420b461e1600f8d3399f7a89ef8d201575
Juan Mercado <juan.mercado1986@gmail.com>
2023-08-21 20:36:16 -0300

    Entrega desafio 2

--- Desafio2/src/model/domain/Alumno.java
+++ Desafio2/src/model/domain/Alumno.java
@@ -3,11 +3,13 @@ package model.domain;
 public class Alumno {
     private String nombre;
     private String apellido;
+    private int numeroDeExamen;
     private double nota;
 
-    public Alumno(String nombre, String apellido, double nota){
+    public Alumno(String nombre, String apellido, int numeroDeExamen, double nota){
         this.nombre = nombre;
         this.apellido = apellido;
+        this.numeroDeExamen = numeroDeExamen;
         this.nota = nota;
     }
 
@@ -19,16 +21,28 @@ public class Alumno {
         return apellido;
     }
 
+    public int getNumeroDeExamen(){
+        return numeroDeExamen;
+    }
+
     public double getNota() {
         return nota;
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return nombre.equals(alumno.getNombre()) && apellido.equals(alumno.getApellido());
+    }
+
+    @Override
     public String toString() {
         return "Alumno{" +
-                "nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", nota=" + nota +
-                '}';
+                "nombre ='" + nombre + '\'' +
+                ", apellido ='" + apellido + '\'' +
+                ", N° de Examen ='" + numeroDeExamen + '\'' +
+                ", nota =" + nota +"}\n";
     }
 }
